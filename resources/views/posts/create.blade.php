8c449ad5bc68ab4071cb392bcce42becae4af6e4
ngungath <daniel2ngungath@gmail.com>
2018-12-08 15:14:19 +0300

    erros styling

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 
 @if(count($errors) > 0)
 
-<ul class="list-group">
+<ul class="list-group" style="color: red;">
 	@foreach($errors->all() as $error)
      <li class="list-group-item">
      	{{$error}}
